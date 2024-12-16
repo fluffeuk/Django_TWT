7b96f3826370b24b8b0fdee4352af912bc9d7107
fluffeuk <agetrosk@gmail.com>
2024-12-16 19:53:34 +0100

    Set up basic inheritance in templates

--- koalasite/main/views.py
+++ koalasite/main/views.py
@@ -5,15 +5,7 @@ from .models import ToDoList, Item
 # Create your views here.
 def index(response, id):
     ls = ToDoList.objects.get(id = id)
-    item = ls.item_set.get(id = 1)
-    return HttpResponse("<h1>%s</h1><br></br><p>%s</p>" %(ls.name, str(item.text)))
+    return render(response, "main/base.html", {})
 
 def home(response):
-    pass
-'''
-what goes between the brackets is html code. we can add tags on either side. if we don't it will be the same as 
-    preformatted (plain) text. we could achieve this in a .html doc by putting text between body tags:
-    <body> koalahhh! </body>
-    or using the pre tag:
-    <pre> koalahhh! </pre>
-'''
+    return render(response, "main/home.html", {})
