1516e65675df505459499d46fcdc576b69eb9e3e
fluffeuk <135436177+fluffeuk@users.noreply.github.com>
2024-12-17 14:10:00 +0100

    Complete simple forms setup

--- koalasite/main/views.py
+++ koalasite/main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 from .models import ToDoList, Item
 from .forms import CreateNewList
+
 
 # Create your views here.
 def index(response, id):
@@ -12,5 +14,18 @@ def home(response):
     return render(response, "main/home.html", {})
 
 def create(response):
+    if response.method ==  "POST":
+        form = CreateNewList(response.POST)
+            
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name=n)
+            t.save()
+    
+        return HttpResponseRedirect("/%i" %t.id)
+    
+    else:
+        form = CreateNewList()
+        
     form = CreateNewList()
     return render(response, "main/create.html", {"form":form})
